94fb57c16bf772ed16b5e79992ede148dc098aba
Bercis-Lisan <bercislisan569@gmail.com>
2026-07-18 23:23:29 +0530

    pre final push

--- product2.js
+++ product2.js
@@ -57,72 +57,6 @@ filterToggle.addEventListener("click", () => {
 const products = [
 
     {
-        image: "images/product8.jpg",
-        tag: "Just In",
-        name: "England 2026 Match Home",
-        content: "Men's Nike Aero-FIT Football Authentic Shirt",
-        price: "₹8,295",
-        category: "men,men-clothing,short sleeve,featured",
-        newest: true,
-    
-    },
-
-    {
-        image: "images/product9.jpg",
-        name: "Brazil 2026 Stadium Home",
-        content: "Men's Nike Dri-FIT Football Replica Shorts",
-        price: "₹2,546",
-        category: "men,men-clothing,featured"
-    },
-
-    {
-        image: "images/product13.jpg",
-        name: "England 2026 Match Home",
-        content: "Men's Nike Aero-FIT Football Authentic Shirt",
-        price: "₹2,546",
-        category: "men,men-clothing,featured"
-    },
-    
-    {
-        image: "images/lifestyle3.jpg",
-        name: "Nike Air Max 95 Big Bubble 'OG'",
-        content: "Men's Shoes",
-        price: "₹14,396",
-        originalPrice: "₹15,995",
-        discount: "10% off",
-        category: "men,women,lifestyle,10% and above,all discounted products,featured"
-    },
-    {
-        image: "images/accessories1.jpg",
-        name: "NK ACDMY TEAM M DUFF",
-        content: "Academy Team Football Duffel Bag (Medium, 60L)",
-        price: "₹2,156",
-        originalPrice: "₹2,695",
-        discount: "20% off",
-        category: "men,men-accessories,women-accessories,20% and above,all discounted products,featured"
-    },
-    {
-        image: "images/accessories2.jpg",
-        name: "Nike",
-        content: "Academic Football Shoe Bag",
-        price: "₹846",
-        category: "men,men-accessories,women-accessories,featured"
-    },
-    {
-        image: "images/accessories3.jpg",
-        name: "Jordan Pro",
-        content: "Nike Jordan Pro Unstructured Flat-Bill Hat",
-        price: "₹1,995",
-        category: "men,men-accessories,women-accessories,featured"
-    },
-    {
-        image: "images/accessories4.jpg",
-        name: "FFF VaporFast Home",
-        content: "Nike FFF VaporFast Home Dri-FIT ADV Football Knee-High Socks",
-        price: "₹1,436",
-        category: "men,men-accessories,women-accessories,featured"
-    },
-    {
         image: "images/women1.jpg",
         name: "Nike Sportswear",
         content: "Women's Loose Lace Polo Jersey Top",
@@ -145,96 +79,65 @@ const products = [
         price: "₹3,141",
         category: "women,women-clothing,featured"
     },
-
-    {
-        image: "images/womenshoes1.jpg",
-        name: "Nike Air Max 95 Big Bubble",
-        content: "Women's Shoes",
-        price: "₹15,995",
-        category: "women,women-shoes,featured"
-    },
-
-    {
-        image: "images/womenshoes2.jpg",
-        name: "Nike Air Max 90",
-        content: "Women's Shoes",
-        price: "₹10,995",
-        category: "women,women-shoes,featured"
-    },
-
-    {
-        image: "images/womenshoes3.jpg",
-        name: "Nike Astrograbber Leather",
-        content: "Women's Shoes",
-        price: "₹7,557",
-        category: "women,women-shoes,featured"
-    },
-    {
-        image: "images/running.jpg",
-        name: "Nike Stride",
-        content: "Men's Dri-FIT 13cm (approx.) Brief-Lined Running Shorts",
-        price: "₹1,797",
-        originalPrice: "₹2,995",
-        discount: "40% off",
-        category: "all discounted products,running,10% and above,20% and above,featured"
-    },
-    {
-        image: "images/running1.jpg",
-        tag: "Just In",
-        name: "Nike Air Max 95 Big Bubble",
-        content: "Women's Shoes with Reflective Design Accents",
-        price: "₹15,995",
-        category: "women,women-shoes,running,featured"
-    },
-    {
-        image: "images/longsleeve.jpg",
-        name: "Brazil Club",
-        content: "Nike Brazil Club Men's Footbal French Terry Pullover Hoodie",
-        price: "₹4,695",
-        category: "long sleeve,featured"
-    },
-    {
-        image: "images/longsleeve1.jpg",
-        name: "FFF Club",
-        content: "Nike FFF Club Men's Football French Terry Pullover Hoodie",
-        price: "₹4,695",
-        category: "long sleeve,featured"
-    },
-    {
-        image: "images/jordan1.jpg",
-        name: "Air Jordan 1 Low",
-        content: "Men's Shoes",
-        price: "₹11,895",
-        category: "men,jordan,featured"
-    },
-    
-    {
-        image: "images/jordan3.jpg",
-        name: "Air Jordan 1 Mid",
-        content: "Men's Shoes",
-        price: "₹12,295",
-        category: "men,jordan,featured"
-    },
-    {
-        image: "images/jordan4.jpg",
-        name: "Air Jordan 3 Retro White Cement",
-        content: "Men's Shoes",
-        price: "₹17,495",
-        category: "men,jordan,featured"
-    },
-    {
-        image: "images/longsleeve2.jpg",
-        name: "Nike Academy",
-        content: "Men's Dri-FIT Football Drill Top",
-        price: "₹2,097",
-        originalPrice: "₹2, 995",
-        discount: "30 % off",
-        category: "long sleeve,featured"
-    },
     {
         image: "images/product10.jpg",
         name: "Brazil 2026 Stadium Home",
         content: "Nike Brazil 2026 Stadium Home Older Kids' Dri-FIT Football Replica Shirt",
+        price: "₹3,995",
+        category: "kids,kids-clothing,short sleeve,featured"
+    },
+    {
+        image: "images/product8.jpg",
+        tag: "Just In",
+        name: "England 2026 Match Home",
+        content: "Men's Nike Aero-FIT Football Authentic Shirt",
+        price: "₹8,295",
+        category: "men,men-clothing,short sleeve,featured",
+        newest: true
+    },
+
+    {
+        image: "images/product9.jpg",
+        name: "Brazil 2026 Stadium Home",
+        content: "Men's Nike Dri-FIT Football Replica Shorts",
+        price: "₹2,546",
+        category: "men,men-clothing,featured"
+    },
+
+    
+
+    {
+        image: "images/product11.jpg",
+        name: "England 2026 Match Home",
+        content: "Older Kids' Nike Dri-FIT Football Replica Shirt",
+        price: "₹3,995",
+        category: "kids,kids-clothing,short sleeve,featured"
+    },
+    {
+        image: "images/product12.jpg",
+        name: "England 2026 Match Home",
+        content: "Younger Kids' Nike Football Replica 3-Piece Kit",
+        price: "₹3,995",
+        category: "kids,kids-clothing,short sleeve,featured"
+    },
+    {
+        image: "images/product13.jpg",
+        name: "England 2026 Match Home",
+        content: "Men's Nike Aero-FIT Football Authentic Shirt",
+        price: "₹2,546",
+        category: "men,men-clothing,featured"
+    },
+    {
+        image: "images/product14.jpg",
+        name: "FFF 2026 Stadium Away",
+        content: "Men's Nike Dri-FIT Football Replica Shorts",
+        price: "₹5,095",
+        category: "men,men-clothing,featured"
+    },
+    {
+        image: "images/product15.jpg",
+        name: "FFF 2026 Stadium Away",
+        content: "Older Kids' Nike Dri-FIT Football Replica Shirt",
         price: "₹3,995",
         category: "kids,kids-clothing,short sleeve,featured"
     },
@@ -309,36 +212,6 @@ const products = [
     },
 
     
-
-    {
-        image: "images/product11.jpg",
-        name: "England 2026 Match Home",
-        content: "Older Kids' Nike Dri-FIT Football Replica Shirt",
-        price: "₹3,995",
-        category: "kids,kids-clothing,short sleeve,featured"
-    },
-    {
-        image: "images/product12.jpg",
-        name: "England 2026 Match Home",
-        content: "Younger Kids' Nike Football Replica 3-Piece Kit",
-        price: "₹3,995",
-        category: "kids,kids-clothing,short sleeve,featured"
-    },
-    
-    {
-        image: "images/product14.jpg",
-        name: "FFF 2026 Stadium Away",
-        content: "Men's Nike Dri-FIT Football Replica Shorts",
-        price: "₹5,095",
-        category: "men,men-clothing,featured"
-    },
-    {
-        image: "images/product15.jpg",
-        name: "FFF 2026 Stadium Away",
-        content: "Older Kids' Nike Dri-FIT Football Replica Shirt",
-        price: "₹3,995",
-        category: "kids,kids-clothing,short sleeve,featured"
-    },
     {
         image: "images/product16.jpg",
         name: "Nike Killshot 2",
@@ -373,6 +246,131 @@ const products = [
         discount: "20% off",
         category: "men,women,lifestyle,10% and above,20% and above,all discounted products,featured"
     },
+    {
+        image: "images/lifestyle3.jpg",
+        name: "Nike Air Max 95 Big Bubble 'OG'",
+        content: "Men's Shoes",
+        price: "₹14,396",
+        originalPrice: "₹15,995",
+        discount: "10% off",
+        category: "men,women,lifestyle,10% and above,all discounted products,featured"
+    },
+    {
+        image: "images/accessories1.jpg",
+        name: "NK ACDMY TEAM M DUFF",
+        content: "Academy Team Football Duffel Bag (Medium, 60L)",
+        price: "₹2,156",
+        originalPrice: "₹2,695",
+        discount: "20% off",
+        category: "men,men-accessories,women-accessories,20% and above,all discounted products,featured"
+    },
+    {
+        image: "images/accessories2.jpg",
+        name: "Nike",
+        content: "Academic Football Shoe Bag",
+        price: "₹846",
+        category: "men,men-accessories,women-accessories,featured"
+    },
+    {
+        image: "images/accessories3.jpg",
+        name: "Jordan Pro",
+        content: "Nike Jordan Pro Unstructured Flat-Bill Hat",
+        price: "₹1,995",
+        category: "men,men-accessories,women-accessories,featured"
+    },
+    {
+        image: "images/accessories4.jpg",
+        name: "FFF VaporFast Home",
+        content: "Nike FFF VaporFast Home Dri-FIT ADV Football Knee-High Socks",
+        price: "₹1,436",
+        category: "men,men-accessories,women-accessories,featured"
+    },
+    
+
+    {
+        image: "images/womenshoes1.jpg",
+        name: "Nike Air Max 95 Big Bubble",
+        content: "Women's Shoes",
+        price: "₹15,995",
+        category: "women,women-shoes,featured"
+    },
+
+    {
+        image: "images/womenshoes2.jpg",
+        name: "Nike Air Max 90",
+        content: "Women's Shoes",
+        price: "₹10,995",
+        category: "women,women-shoes,featured"
+    },
+
+    {
+        image: "images/womenshoes3.jpg",
+        name: "Nike Astrograbber Leather",
+        content: "Women's Shoes",
+        price: "₹7,557",
+        category: "women,women-shoes,featured"
+    },
+    {
+        image: "images/running.jpg",
+        name: "Nike Stride",
+        content: "Men's Dri-FIT 13cm (approx.) Brief-Lined Running Shorts",
+        price: "₹1,797",
+        originalPrice: "₹2,995",
+        discount: "40% off",
+        category: "all discounted products,running,10% and above,20% and above,featured"
+    },
+    {
+        image: "images/running1.jpg",
+        tag: "Just In",
+        name: "Nike Air Max 95 Big Bubble",
+        content: "Women's Shoes with Reflective Design Accents",
+        price: "₹15,995",
+        category: "women,women-shoes,running,featured"
+    },
+    {
+        image: "images/longsleeve.jpg",
+        name: "Brazil Club",
+        content: "Nike Brazil Club Men's Footbal French Terry Pullover Hoodie",
+        price: "₹4,695",
+        category: "long sleeve,featured"
+    },
+    {
+        image: "images/longsleeve1.jpg",
+        name: "FFF Club",
+        content: "Nike FFF Club Men's Football French Terry Pullover Hoodie",
+        price: "₹4,695",
+        category: "long sleeve,featured"
+    },
+    {
+        image: "images/jordan1.jpg",
+        name: "Air Jordan 1 Low",
+        content: "Men's Shoes",
+        price: "₹11,895",
+        category: "men,jordan,featured"
+    },
+    {
+        image: "images/jordan3.jpg",
+        name: "Air Jordan 1 Mid",
+        content: "Men's Shoes",
+        price: "₹12,295",
+        category: "men,jordan,featured"
+    },
+    {
+        image: "images/jordan4.jpg",
+        name: "Air Jordan 3 Retro White Cement",
+        content: "Men's Shoes",
+        price: "₹17,495",
+        category: "men,jordan,featured"
+    },
+    {
+        image: "images/longsleeve2.jpg",
+        name: "Nike Academy",
+        content: "Men's Dri-FIT Football Drill Top",
+        price: "₹2,097",
+        originalPrice: "₹2, 995",
+        discount: "30 % off",
+        category: "long sleeve,featured"
+    }
 
 ];
 
@@ -412,7 +410,7 @@ function displayProducts(items) {
            <span class="original-price">${product.originalPrice}</span>
             <span class="discount">${product.discount}</span> ` : ""
             }
-     </div>
+        </div>
 
             <button class="add-to-cart" style="background-color: black; color: white; padding: 5px; border: 1px solid black; border-radius: 5px;">Add to Cart</button>
         </div>
@@ -499,5 +497,4 @@ function sortProducts(type) {
     sortMenu.classList.remove("show");
 
 }
-
 
